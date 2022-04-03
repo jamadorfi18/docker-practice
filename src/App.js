7ebda1ac18fc4be44d80846a6bc6f04cae6a6363
Amador Flores <jamador@adda.mx>
2022-04-02 19:47:34 -0600

    Added docker config

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          My first docker configuration using react with dev and prod enviroment!
         </p>
         <a
           className="App-link"
